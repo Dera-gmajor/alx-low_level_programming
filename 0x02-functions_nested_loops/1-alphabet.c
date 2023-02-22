da928e04cf0cb65e8b0633c656b8684dcdbf3365
Dera-gmajor <ibezimderarita@gmail.com>
2023-02-21 23:01:13 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,16 @@
 
 /**
  * print_alphabet - print alphabet
- *
  * Return: void
- *
  */
 
 void print_alphabet(void)
 {
 
-	char letter = 'a';
-	while (letter <= 'z')
-{
-	_putchar(letter);
-	letter++;
+	char c;
+	for (c= 'a'; c= 'z'; c++)
+{ 
+_putchar(c);
 }
 _putchar('\n');
 }
