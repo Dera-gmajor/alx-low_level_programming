e9d02dede830a0f805bb10563e09b2b52fa17d0a
Dera-gmajor <ibezimderarita@gmail.com>
2023-02-23 01:18:02 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,7 +19,7 @@ void print_times_table(int n)
 				{
 					_putchar(k + '0');
 				} else if (k < 10 && j != 0)
-			        {
+				{
 
 					_putchar(',');
 					_putchar(' ');
@@ -34,15 +34,15 @@ void print_times_table(int n)
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				} else if (k >= 100)
-			        {
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
-		         }
-		         _putchar('\n');
+			}
+				_putchar('\n');
 		}
 	}
 }
