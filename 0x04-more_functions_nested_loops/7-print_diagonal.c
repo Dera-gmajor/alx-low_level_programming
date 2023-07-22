c302f63e909873b18f99ecd27adec911dec75eb7
Dera-gmajor <ibezimderarita@gmail.com>
2023-07-22 13:36:12 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,18 +5,25 @@
  */
 void print_diagonal(int n)
 {
-	int x;
-	int y;
+	int b;
+	int c;
 
 	if (n <= 0)
-		_putchar('\n');
-	for (x = 0; x < n; ++x)
 	{
-		for (y = 0; y < n; ++y)
-		{
-			_putchar(' ');
-		}
-		_putchar('\\');
 		_putchar('\n');
+	}
+	else
+	{
+		for (b = 0; b < n; b++)
+		{
+			for (c = 0; c < n; c++)
+			{
+				if (c == b)
+					_putchar('\\');
+				else if (c < b)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
 	}
 }
