5d68d342afae5c4e116ccfe4be2e1ee76b559c4e
Dera-gmajor <ibezimderarita@gmail.com>
2023-07-27 15:03:01 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings.
+ *  @s1: string 1
+ *  @s2: string 2
+ *  Return: 0 if s1 == s2, positive if s1 > s2, negative if s1 < s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int y;
+
+	y = 0;
+	while (s1[y] == s2[y] && s1[y] != '\0')
+	{
+		y++;
+	}
+	if (s1[y] != s2[y])
+		return (s1[y] - s2[y]);
+}
