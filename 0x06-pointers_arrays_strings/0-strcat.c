a2e445f8a3b0c368e42d9d32cd0ce684b84de4d0
Dera-gmajor <ibezimderarita@gmail.com>
2023-07-27 13:33:13 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
  * *_strcat - concatenates two strings
  * @dest: character strings
  * @src: character strings to concatenate to
- * Retuen: to the dest strings
+ * Return: to the dest strings
  */
 char *_strcat(char *dest, char *src)
 {
@@ -13,9 +13,10 @@ char *_strcat(char *dest, char *src)
 	for (d = 0; dest[d] != '\0'; d++)
 		;
 
-	for (s = 0; src[s] != '\0'; s++, d++)
+	for (s = 0; src[s] != '\0'; s++)
 	{
 		dest[d] = src[s];
+		d++;
 	}
 	dest[d] = '\0';
 	return (dest);
