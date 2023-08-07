cf7e043a81aa92d3b99aeab9bc921328ee2b866a
Dera-gmajor <ibezimderarita@gmail.com>
2023-08-07 15:09:54 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: array of character pointers
  * Return: Always 0
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
