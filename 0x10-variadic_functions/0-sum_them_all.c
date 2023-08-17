2a943642ba497b2c13b8c330c50792968015fc04
Dera-gmajor <ibezimderarita@gmail.com>
2023-08-17 14:09:20 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,24 +7,24 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
+	if (n != 0)
 	{
-		return (0);
+
+		int sum = 0;
+		unsigned int g = 0;
+		va_list lists;
+
+		va_start(lists, n);
+
+		while (g < n)
+		{
+			int num = va_arg(lists, int);
+
+			sum += num;
+			g++;
+		}
+		va_end(lists);
+		return (sum);
 	}
-
-	int sum = 0;
-	unsigned int g = 0;
-	va_list lists;
-
-	va_start(lists, n);
-
-	while (g < n)
-	{
-		int num = va_arg(lists, int);
-
-		sum += num;
-		g++;
-	}
-	va_end(lists);
-	return (sum);
+	return (0);
 }
