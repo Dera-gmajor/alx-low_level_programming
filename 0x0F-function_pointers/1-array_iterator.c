f964d4c13ae44c32275285aea5f08d95457ba1bd
Dera-gmajor <ibezimderarita@gmail.com>
2023-08-27 09:07:22 -0700

    array iterating

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int m;
 
-	if (array && action && size > 0)
+	if (!(!array || !action || size <= 0))
 	{
 		for (i = 0; i < size; i++)
-		{
 			action(array[m]);
-		}
 	}
 }
