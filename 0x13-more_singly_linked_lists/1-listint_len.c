024b8b408f2f493cc3382c5554e7d32c2160b3d2
Dera-gmajor <ibezimderarita@gmail.com>
2023-08-29 21:36:12 -0700

    singly

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: list head
+ * Return: number of element
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t a;
+	const listint_t *b;
+
+	for (b = h, a = 0; b != NULL; b = b->next, a++)
+	{
+		// Empty loop body
+	}
+	return (a);
+}
