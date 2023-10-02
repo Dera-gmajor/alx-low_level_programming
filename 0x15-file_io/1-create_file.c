9569469b0d959e3731e31b5c359629e080fa50b2
Dera-gmajor <ibezimderarita@gmail.com>
2023-10-02 13:09:45 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,22 +13,22 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-		a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-		if (a == -1)
-			return (-1);
-		if (!text_content)
-			text_content = "";
-		b = 0;
+	a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (a == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	b = 0;
+	while (text_content[b])
+	{
+		b++;
+	}
+	c = write(a, text_content, b);
 
-		while (text_content[b])
-		{
-			b++;
-		}
-		c = write(a, text_content, b);
+	if (c == -1)
+		return (-1);
 
-		if (c == -1)
-			return (-1);
-		close(a);
-		return (1);
+	close(a);
+	return (1);
 }
